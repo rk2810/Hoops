c9f7874cdc844cb81fc44fe57f4ec508f7f10c05
rk2810 <rohan.kumquat@gmail.com>
2018-08-06 11:14:20 +0530

    Mundane initial commit.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

---
+++ app/src/main/java/com/jade/rohank/hoops/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.jade.rohank.hoops
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_screen)
+    }
+}
